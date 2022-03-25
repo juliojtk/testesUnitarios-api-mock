3637178c71c8f5a070ac6696bd806751bc489176
juliojtk <julio.kuniwake@gmail.com>
2022-03-25 00:14:44 -0300

    Inicio Projeto

---
+++ src/main/java/com/kuniwake/julio/apimock/ApiMockApplication.java
@@ -0,0 +1,13 @@
+package com.kuniwake.julio.apimock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuniwake/julio/apimock/ApiMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuniwake.julio.apimock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
