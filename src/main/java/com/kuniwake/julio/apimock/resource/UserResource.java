0bd0134a8b55152e14432e08dc458cd78165075a
juliojtk <julio.kuniwake@gmail.com>
2022-03-26 12:00:13 -0300

    EndPoint FindAllUser

--- src/main/java/com/kuniwake/julio/apimock/resource/UserResource.java
+++ src/main/java/com/kuniwake/julio/apimock/resource/UserResource.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -25,5 +28,13 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDto> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDto.class));// mapper.map(), faz com que as informações do User seja informada no UserDto
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> findAll(){
+        List<UserDto> lUserDto = userService.findAllUser()
+                .stream().map(user -> mapper.map(user, UserDto.class)) // Passa as informações de User para UserDto
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(lUserDto);
     }
 }
--- src/main/java/com/kuniwake/julio/apimock/services/UserService.java
+++ src/main/java/com/kuniwake/julio/apimock/services/UserService.java
@@ -1,7 +1,12 @@
 package com.kuniwake.julio.apimock.services;
 
 import com.kuniwake.julio.apimock.domain.User;
+import com.kuniwake.julio.apimock.domain.dto.UserDto;
+
+import java.util.List;
 
 public interface UserService {
     User findById(Integer id);
+
+    List<User> findAllUser();
 }
